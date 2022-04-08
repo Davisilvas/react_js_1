18ab344ec75af3d24975e7aba9cf7d4059cb528d
Davi Silva Santos <morg.daviss@gmail.com>
2022-04-07 21:32:51 -0300

    1

---
+++ index.js
@@ -0,0 +1,9 @@
+const root = document.querySelector("#root");
+
+const t1 = React.createElement('h1', {id: 'main title'}, 'Hello World');
+const t2 = React.createElement('h3', {id: 'subtitle'}, 'My first steps in react js');
+
+const t3 = React.createElement('div', {id: 'container'}, t1, t2);
+
+
+ReactDOM.render(t3, root);
