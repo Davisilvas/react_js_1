2ab9cd8d5b6f86800fabf54633d2fb1f64f88064
Davi Silva Santos <morg.daviss@gmail.com>
2022-04-27 21:35:07 -0300

    aula 9 estados

--- new-try/src/App.js
+++ new-try/src/App.js
@@ -1,12 +1,19 @@
 // import logo from './logo.svg';
 import Header from './Header';
 import React from 'react';
+import Counter from './Counter'
 import './App.css';
 
 function App() {
   return (
-      <Header name='Davi'
-      links={['about', 'buy', 'contact', 'login']}></Header>);
+      <div>
+        <Header name='Davi'
+        links={['about', 'buy', 'contact', 'login']}></Header>
+        {/* <Counter count={7}></Counter> */}
+        <Counter></Counter>
+
+      </div>
+      );
 }
 
 export default App;
--- new-try/src/Header.js
+++ new-try/src/Header.js
@@ -2,20 +2,13 @@ import React from "react";
 import Menu from "./menu";
 
 
-class Header extends React.Component{
 
-    constructor(props){
-        super(props)
-    }
-    
-    render(){
-        return(
-            <header className="header">
-                <h1>{this.props.name}</h1>
-                <Menu links = {this.props.links}></Menu>
-            </header>)
-    }
+function Header(props){
+    return(
+        <header className="header">
+            <h1>{props.name}</h1>
+            <Menu links = {props.links}></Menu>
+        </header>)
 }
-
 
 export default Header;
